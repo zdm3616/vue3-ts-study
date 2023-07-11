c543210ec1aeb2f15f5c8c05f550d4f4c0772d87
zdm <1746137876@qq.com>
2023-07-11 17:51:51 +0800

    新建项目

---
+++ vite-project/src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import { Check } from '@element-plus/icons-vue'
+</script>
+<template>
+  <el-button :icon="Check" type="primary">111</el-button>
+</template>
+
+<style scoped></style>
